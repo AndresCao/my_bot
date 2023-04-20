6ec082076c4fb04bd37c1bfab115c9eb01b206c5
Andres <andrescao66@gmail.com>
2023-04-20 14:06:34 -0400

    launch everything on the robot at the same time

---
+++ launch/launch_robot_everything.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+
+
+def generate_launch_description():
+
+    # include another launch file
+    launch_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('my_bot'),
+                'launch/launch_robot.launch.py'))
+    )
+    launch_rplidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('my_bot'),
+                'launch/rplidar.launch.py'))
+    )
+    return LaunchDescription([
+
+        launch_robot,
+        launch_rplidar,
+    ])
